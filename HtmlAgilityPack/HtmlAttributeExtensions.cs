75d80e4854b2896358a098072bbdde57d1287dff
aleksandar.hitrov@telerik.com <aleksandar.hitrov@telerik.com>
2016-09-06 10:32:13 +0300

    Expose inner properties via extension methods
    The content public properties ```OuterHtml``` and ```InnerHtml``` is optimized HTML where e.g.: all spaces between attributes are compacted into a single white-space. As a result we can't navigate in the HTML tree using a stream position.
    
    For that reason we are expose the indexes calculated during the parse.

---
+++ HtmlAgilityPack/HtmlAttributeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace HtmlAgilityPack
+{
+	public static class HtmlAttributeExtensions
+	{
+		public static int GetValueStartIndex(this HtmlAttribute attribute)
+		{
+			return attribute._valuestartindex;
+		}
+
+		public static int GetValueLength(this HtmlAttribute attribute)
+		{
+			return attribute._valuelength;
+		}
+	}
+}
